a3ca65c236d6cc55f946af5fe340eb8e88ed7b58
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-08-28 14:34:41 -0400

    created post,put,delete routes

--- server.js
+++ server.js
@@ -6,11 +6,33 @@ const app = express(); // calling express to use in the future
 
 const port = process.env.PORT; // using port from our .env file
 
+app.use(express.json()); // middleware to parse json data
+
 let fruitData = ["apple", "Pear", "Golden Kiwi", "Mango"];
 
 app.get("/", (req, res) => {
-  //   res.send("Hello World");
-  res.json("Hello World");
+  res.json(fruitData); // send the fruitData to the client
+});
+
+app.get("/:id", (req, res) => {
+  let data = fruitData[req.params.id];
+  res.json(data);
+});
+
+app.post("/", (req, res) => {
+  let data = req.body; // grabbing the data the client entered from the req.body
+  fruitData.push(data.fruit); // pushing the data into fruitData
+  res.json(fruitData); // send the updated fruitData as a json
+});
+
+app.put("/:id", (req, res) => {
+  let data = fruitData[req.params.id];
+  res.json(data);
+});
+
+app.delete("/:id", (req, res) => {
+  let data = fruitData[req.params.id];
+  res.json(data);
 });
 
 // our express sever is listening in port 3000
