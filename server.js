f05a71602fe524b221d57bb91765ceff928e6203
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-08-28 14:07:13 -0400

    created get route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express"); // importing express
+const dotenv = require("dotenv"); // import dotenv
+dotenv.config(); // using dotenv
 
 const app = express(); // calling express to use in the future
 
-const port = 3000; // creating a port
+const port = process.env.PORT; // using port from our .env file
+
+let fruitData = ["apple", "Pear", "Golden Kiwi", "Mango"];
+
+app.get("/", (req, res) => {
+  //   res.send("Hello World");
+  res.json("Hello World");
+});
 
 // our express sever is listening in port 3000
 app.listen(port, () => {
