a70f928231659327b4b6e03c85f6c3fc804661a0
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-08-28 13:43:16 -0400

    starter code for creating a server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require("express"); // importing express
 
+const app = express(); // calling express to use in the future
+
+const port = 3000; // creating a port
+
+// our express sever is listening in port 3000
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
